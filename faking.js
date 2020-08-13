103f210d892884a60f2b8c876fad7100470432ea
glitchgirl <londonspleen@gmail.com>
2020-08-13 14:48:30 -0400

    scripts

--- faking.js
+++ faking.js
@@ -3,12 +3,13 @@ var faker = require('faker');
 var name = faker.name.findName(); 
 var email = faker.internet.email(); 
 var randomPic = faker.image.avatar(); 
+var address = faker.address.city()
 
 function makeCard(){
-
-document.getElementById("picture").src = randomPic;
-document.getElementById("name").innerHTML = name;
-document.getElementById("email").innerHTML = email;
+    document.getElementById("picture").src = randomPic;
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("email").innerHTML = email;
+    document.getElementById("city").innerHTML = address;
 }
 
 makeCard();
